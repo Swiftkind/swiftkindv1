e63acc31988177ce6a622536fae40ca65fd492e7
jkingz <jfkrule@gmail.com>
2017-09-21 13:48:00 +0800

    Create new migration file

--- pages/admin.py
+++ pages/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'subject']
+    list_display = ['name', 'email', 'subject', 'date']
 
 admin.site.register(Contact, ContactAdmin)
--- pages/models.py
+++ pages/models.py
@@ -6,6 +6,7 @@ class Contact(models.Model):
     email = models.EmailField(max_length=30, unique=True)
     subject = models.CharField(max_length=200)
     budget = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField()
 
     def __str__(self):
         return "{}".format(self.name)
