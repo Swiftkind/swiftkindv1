1898f70325f327b594ba585635214818f812f9f9
jkingz <jfkrule@gmail.com>
2017-09-21 12:53:49 +0800

    Create new migration and models for contact page

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=30, blank=True, null=True)
+    email = models.EmailField(max_length=30, unique=True)
+    subject = models.CharField(max_length=200)
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return "{}".format(self.name)
