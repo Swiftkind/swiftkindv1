534c45eff5a7f17cbf32a8c42d0a73a29b739434
resty <resty.antiquin@gmail.com>
2017-10-31 16:53:17 +0800

    fixed templates

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,7 +127,7 @@ $(document).ready(function() {
     $(function() {
 		$.scrollify({
 			section : ".section",
-            sectionName : "section-name",
+            sectionName : false,
 		});
 	});
 
